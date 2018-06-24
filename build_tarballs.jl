5faea0ed96d4302df12b5b8dc9a8d02545a1a56e
Kenta Sato <bicycle1885@gmail.com>
2018-06-24 17:42:59 +0900

    add some touch-ups

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -1,7 +1,9 @@
 using BinaryBuilder
 
+version = v"2.9.7"
+
 sources = [
-    "https://github.com/GNOME/libxml2/archive/v2.9.7.tar.gz" =>
+    "https://github.com/GNOME/libxml2/archive/v$(version).tar.gz" =>
         "a7b52278afca823d263deb8b4b33710735f1b5208a351173b2deb78a6d936412",
 ]
 
@@ -19,7 +21,7 @@ products(prefix) = [
 platforms = supported_platforms()
 
 dependencies = [
-    "https://github.com/bicycle1885/ZlibBuilder/releases/download/v1.0.0/build.jl",
+    "https://github.com/bicycle1885/ZlibBuilder/releases/download/v1.0.1/build_Zlib.v1.2.11.jl",
 ]
 
-build_tarballs(ARGS, "XML2Builder", sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, "XML2Builder", version, sources, script, platforms, products, dependencies)
