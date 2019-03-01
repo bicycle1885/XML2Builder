32dcf0cdc6146a8e4b968a143d82e35ff4d98a22
Kenta Sato <bicycle1885@gmail.com>
2019-03-02 01:50:32 +0900

    bump libxml2 and zlib

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -1,12 +1,17 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder
 
-version = v"2.9.7"
+name = "XML2Builder"
+version = v"2.9.9"
 
+# Collection of sources required to build XML2Builder
 sources = [
     "https://github.com/GNOME/libxml2/archive/v$(version).tar.gz" =>
-        "a7b52278afca823d263deb8b4b33710735f1b5208a351173b2deb78a6d936412",
+    "d673f0284cec867ee00872a8152e0c3c09852f17fd9aa93f07579a37534f0bfe",
 ]
 
+# Bash recipe for building across all platforms
 script = raw"""
 cd ${WORKSPACE}/srcdir/libxml2-*
 ./autogen.sh
@@ -14,14 +19,19 @@ cd ${WORKSPACE}/srcdir/libxml2-*
 make -j${nproc} install
 """
 
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
 products(prefix) = [
     LibraryProduct(prefix, "libxml2", :libxml2)
 ]
 
-platforms = supported_platforms()
-
+# Dependencies that must be installed before this package can be built
 dependencies = [
-    "https://github.com/bicycle1885/ZlibBuilder/releases/download/v1.0.1/build_Zlib.v1.2.11.jl",
+    "https://github.com/bicycle1885/ZlibBuilder/releases/download/v1.0.4/build_Zlib.v1.2.11.jl"
 ]
 
-build_tarballs(ARGS, "XML2Builder", version, sources, script, platforms, products, dependencies)
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
